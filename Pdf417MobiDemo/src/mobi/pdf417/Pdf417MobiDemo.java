7ecf289dddea5d6b373dc72098d56d7eb4fdddc3
DoDo <DoDoEntertainment@gmail.com>
2013-09-21 22:11:36 +0200

    [Pdf417MobiSdk] fixed memory leak when multiple scans were performed in short timeframe; fixed possible deadlock in native library initialization

--- Pdf417MobiDemo/src/mobi/pdf417/Pdf417MobiDemo.java
+++ Pdf417MobiDemo/src/mobi/pdf417/Pdf417MobiDemo.java
@@ -17,9 +17,9 @@ import android.view.WindowManager;
 
 public class Pdf417MobiDemo extends Activity {
     
-    public static final String TAG = "MainActivity";
-    
     private static final int MY_REQUEST_CODE = 1337;
+    
+    private static final String TAG = "Pdf417MobiDemo";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +37,9 @@ public class Pdf417MobiDemo extends Activity {
         Intent intent = new Intent(this, Pdf417ScanActivity.class);
         // add a resource id for sound that will be played on successful scan (optional)
         intent.putExtra(Pdf417ScanActivity.EXTRAS_BEEP_RESOURCE, R.raw.beep);
+        // if you want to use front facing camera instead of back facing
+        // uncoment the following line
+//        intent.putExtra(Pdf417ScanActivity.EXTRAS_CAMERA_TYPE, (Parcelable)CameraType.CAMERA_FRONTFACE);
         // If you want to open front facing camera, uncomment the following line.
         // Note that front facing cameras do not have autofocus support, so it will not
         // be possible to scan denser and smaller codes.
