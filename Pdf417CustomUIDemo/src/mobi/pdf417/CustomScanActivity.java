d58df4feb6f3c0919274efb6608e6eba4f762e0f
DoDo <DoDoEntertainment@gmail.com>
2014-02-10 15:36:19 +0100

    [Pdf417CustomUIDemo] ROI example

--- Pdf417CustomUIDemo/src/mobi/pdf417/CustomScanActivity.java
+++ Pdf417CustomUIDemo/src/mobi/pdf417/CustomScanActivity.java
@@ -4,75 +4,83 @@ import mobi.pdf417.viewfinder.MyViewfinder;
 import net.photopay.base.BaseBarcodeActivity;
 import net.photopay.view.viewfinder.AbstractViewFinder;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Toast;
 
 public class CustomScanActivity extends BaseBarcodeActivity {
 
-	private int mScanCount = 0;
-	private Handler mHandler = new Handler();
-	MyViewfinder mViewfinder;
-	
-	/**
-	 * This method must create and return custom viewfinder object that
-	 * contains information about how views are layouted on camera surface
-	 * and how they are rotated.
-	 */
-	protected AbstractViewFinder onCreateViewFinder() {
-		LayoutInflater inflater = getLayoutInflater();
-		View rotatableCameraOverlay = inflater.inflate(R.layout.camera_overlay_layout, null);
-		mViewfinder = new MyViewfinder(this, rotatableCameraOverlay);
-		return mViewfinder;
-	}
-	
-	@Override
-	protected void onSetupViewFinder(AbstractViewFinder viewfinder) {
-		super.onSetupViewFinder(viewfinder);
-		mViewfinder.setupViewfinder();
-	}
+    private int mScanCount = 0;
+    private Handler mHandler = new Handler();
+    private MyViewfinder mViewfinder;
+    private boolean mRotateRoi = false;
+    
+    @Override
+    protected void onConfigureWindow() {
+        super.onConfigureWindow();
+        Bundle extras = getIntent().getExtras();
+        if(extras!=null) {
+            mRotateRoi = extras.getBoolean(BaseBarcodeActivity.EXTRAS_ROTATE_ROI, false);
+        }
+    }
 
-	/**
-	 * this activity will perform 5 scans of barcode and then return the last
-	 * scanned one
-	 */
-	@Override
-	protected void onScanningDone(Pdf417MobiScanData scanData) {
-		mScanCount++;
-		StringBuilder sb = new StringBuilder();
-		sb.append("Scanned ");
-		sb.append(mScanCount);
-		switch (mScanCount) {
-		case 1:
-			sb.append("st");
-			break;
-		case 2:
-			sb.append("nd");
-			break;
-		case 3:
-			sb.append("rd");
-			break;
-		default:
-			sb.append("th");
-			break;
-		}
-		sb.append(" barcode!");
-		Toast.makeText(this, sb.toString(), Toast.LENGTH_SHORT).show();
-		if (mScanCount >= 5) {
-			Intent intent = new Intent();
-			intent.putExtra(BaseBarcodeActivity.EXTRAS_RESULT, scanData);
-			setResult(BaseBarcodeActivity.RESULT_OK, intent);
-			finish();
-		} else {
-			mHandler.postDelayed(new Runnable() {
+    
+    /**
+     * This method must create and return custom viewfinder object that contains
+     * information about how views are layouted on camera surface and how they
+     * are rotated.
+     */
+    protected AbstractViewFinder onCreateViewFinder() {
+        mViewfinder = new MyViewfinder(this, mRotateRoi);
+        return mViewfinder;
+    }
 
-				@Override
-				public void run() {
-					resumeScanning();
-				}
-			}, 2000);
-		}
-	}
+    @Override
+    protected void onSetupViewFinder(AbstractViewFinder viewfinder) {
+        super.onSetupViewFinder(viewfinder);
+        mViewfinder.setupViewfinder();
+    }
+
+    /**
+     * this activity will perform 5 scans of barcode and then return the last
+     * scanned one
+     */
+    @Override
+    protected void onScanningDone(Pdf417MobiScanData scanData) {
+        mScanCount++;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Scanned ");
+        sb.append(mScanCount);
+        switch (mScanCount) {
+        case 1:
+            sb.append("st");
+            break;
+        case 2:
+            sb.append("nd");
+            break;
+        case 3:
+            sb.append("rd");
+            break;
+        default:
+            sb.append("th");
+            break;
+        }
+        sb.append(" barcode!");
+        Toast.makeText(this, sb.toString(), Toast.LENGTH_SHORT).show();
+        if (mScanCount >= 5) {
+            Intent intent = new Intent();
+            intent.putExtra(BaseBarcodeActivity.EXTRAS_RESULT, scanData);
+            setResult(BaseBarcodeActivity.RESULT_OK, intent);
+            finish();
+        } else {
+            mHandler.postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    resumeScanning();
+                }
+            }, 2000);
+        }
+    }
 
 }
--- Pdf417CustomUIDemo/src/mobi/pdf417/viewfinder/MyViewfinder.java
+++ Pdf417CustomUIDemo/src/mobi/pdf417/viewfinder/MyViewfinder.java
@@ -3,57 +3,93 @@ package mobi.pdf417.viewfinder;
 import mobi.pdf417.R;
 import net.photopay.view.viewfinder.AbstractViewFinder;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
 
 public class MyViewfinder extends AbstractViewFinder {
-    
+
     private View mLayout;
     private Activity mActivity;
     private Button mBackButton;
     private Button mTorchButton;
     private boolean mTorchEnabled = false;
-    
-    public MyViewfinder(Activity myActivity, View layout) {
-        mLayout = layout;
+
+    private View mRoiLayout;
+    private boolean mRotateRoi = false;
+
+    private FrameLayout mLayoutHolder = null;
+
+    public MyViewfinder(Activity myActivity, boolean rotateRoi) {
         mActivity = myActivity;
-        mBackButton = (Button)mLayout.findViewById(R.id.btnBack);
+        LayoutInflater inflater = mActivity.getLayoutInflater();
+        mLayout = inflater.inflate(R.layout.camera_overlay_layout, null);
+        mRoiLayout = inflater.inflate(R.layout.roi_overlay, null);
+        mRotateRoi = rotateRoi;
+
+        mBackButton = (Button) mLayout.findViewById(R.id.btnBack);
         mBackButton.setOnClickListener(new View.OnClickListener() {
-            
+
             @Override
             public void onClick(View v) {
                 mActivity.finish();
             }
         });
-        
-        mTorchButton = (Button)mLayout.findViewById(R.id.btnTorch);
+
+        mTorchButton = (Button) mLayout.findViewById(R.id.btnTorch);
         mTorchButton.setVisibility(View.GONE);
     }
-    
+
     public void setupViewfinder() {
-        if(isCameraTorchSupported()) {
-        	mTorchButton.setVisibility(View.VISIBLE);
-        	mTorchButton.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					boolean success = setTorchEnabled(!mTorchEnabled);
-					if(success) {
-						mTorchEnabled = !mTorchEnabled;
-					}
-					if(mTorchEnabled) {
-						mTorchButton.setText(R.string.LightOn);
-					} else {
-						mTorchButton.setText(R.string.LightOff);
-					}
-				}
-			});
+        if (isCameraTorchSupported()) {
+            mTorchButton.setVisibility(View.VISIBLE);
+            mTorchButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    boolean success = setTorchEnabled(!mTorchEnabled);
+                    if (success) {
+                        mTorchEnabled = !mTorchEnabled;
+                    }
+                    if (mTorchEnabled) {
+                        mTorchButton.setText(R.string.LightOn);
+                    } else {
+                        mTorchButton.setText(R.string.LightOff);
+                    }
+                }
+            });
         }
     }
-    
+
+    @SuppressWarnings("deprecation")
     @Override
     public View getRotatableView() {
-        return mLayout;
+        // if we want to trotate ROI, then we must pack roi layout and button layout into a
+        // single view and return it. In this example we will pack them in FrameLayout.
+        if (mRotateRoi) {
+            if (mLayoutHolder == null) {
+                mLayoutHolder = new FrameLayout(mActivity);
+                mLayoutHolder.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.FILL_PARENT,
+                        LayoutParams.FILL_PARENT));
+                mLayoutHolder.addView(mRoiLayout);
+                mLayoutHolder.addView(mLayout);
+            }
+            return mLayoutHolder;
+        } else {
+            return mLayout;
+        }
+    }
+
+    @Override
+    public View getFixedView() {
+        // if we do not want to rotate ROI, then return roi layout as fixed view
+        // fixed view will not be rotated when device orientation change occurs
+        if (!mRotateRoi) {
+            return mRoiLayout;
+        } else {
+            return null;
+        }
     }
 }
